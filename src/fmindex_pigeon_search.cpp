670b2411602523beb0e637f89811169eba5d3484
Matthew Martin <matthew@mttmartin.com>
2025-01-23 19:50:42 +0100

    initial version of fmindex pigeon search

--- src/fmindex_pigeon_search.cpp
+++ src/fmindex_pigeon_search.cpp
@@ -1,4 +1,8 @@
 #include <sstream>
+#include <ranges>
+#include <algorithm>
+#include <tuple>
+#include <unordered_set>
 
 #include <seqan3/alphabet/nucleotide/dna5.hpp>
 #include <seqan3/argument_parser/all.hpp>
@@ -6,6 +10,26 @@
 #include <seqan3/io/sequence_file/all.hpp>
 #include <seqan3/search/fm_index/fm_index.hpp>
 #include <seqan3/search/search.hpp>
+
+struct match_hash { 
+  size_t operator()(const std::tuple<int, int, int> &val) const { 
+    return std::get<0>(val) ^ std::get<1>(val) ^ std::get<2>(val); 
+  } 
+}; 
+
+bool verify(std::vector<seqan3::dna5> const& ref, std::vector<seqan3::dna5> const& query, int start_position, int max_mismatches) {
+    int mismatches = 0;
+    for (auto j = 0; j < query.size(); j++) {
+	if (mismatches > max_mismatches)
+		break;
+
+	if (ref[start_position+j] != query[j]) {
+		mismatches++;
+	}
+    }
+
+    return mismatches <= max_mismatches;
+}
 
 int main(int argc, char const* const* argv) {
     seqan3::argument_parser parser{"fmindex_pigeon_search", argc, argv, seqan3::update_notifications::off};
@@ -71,14 +95,67 @@ int main(int argc, char const* const* argv) {
     queries.resize(number_of_queries); // will reduce the amount of searches
 
     seqan3::configuration const cfg = seqan3::search_cfg::max_error_total{seqan3::search_cfg::error_count{0}};
-    //!TODO !ImplementMe use the seqan3::search to find a partial error free hit, verify the rest inside the text
-    // Pseudo code (might be wrong):
-    // for query in queries:
-    //      parts[3] = cut_query(3, query);
-    //      for p in {0, 1, 2}:
-    //          for (pos in search(index, part[p]):
-    //              if (verify(ref, query, pos +- ....something)):
-    //                  std::cout << "found something\n"
+
+
+    for (auto& query : queries) {
+	std::unordered_set<std::tuple<int, int, int>, match_hash> match_results;
+	int piece_size = query.size()/(number_of_errors+1);
+	int first_offset = query.size() % (number_of_errors+1);
+	std::vector<std::span<seqan3::dna5>> pieces;
+	for (auto i = 0; i < (number_of_errors+1); i++) {
+		int start;
+		int end;
+		if (i == 0) {
+			start = i*piece_size;
+			end=piece_size+first_offset;
+		} else {
+			start = (i*piece_size)+first_offset;
+			end=piece_size;
+		}
+		auto piece = std::views::counted(query.begin()+start, end);
+		pieces.push_back(piece);
+	}
+	auto results = seqan3::search(pieces, index, cfg);
+
+    	for (auto && result : results) {
+		match_results.insert(std::make_tuple(result.reference_begin_position(), result.query_id(), result.reference_id()));
+	}
+
+	for (auto& [match_position, piece_id, reference_id] : match_results) {
+		// if we cannot have possibly found a match within reference bounds then skip
+		if ( ((match_position - ((piece_id*piece_size)+first_offset)) < 0) || (match_position + (piece_id*piece_size)+first_offset > reference[reference_id].size())) {
+			continue;
+		}
+
+		int matched_pieces = 0;
+		for (auto i=0; i < piece_id; i++) {
+			int offset = piece_size * (piece_id - i);
+			if (i == 0)
+				offset += first_offset;
+			
+			if (match_results.contains(std::make_tuple(match_position-offset, i, reference_id))) {
+				matched_pieces++;
+			} 
+		}
+
+		for (auto i=piece_id+1; i < pieces.size(); i++) {	
+			if (match_results.contains(std::make_tuple(match_position+(piece_size*i), i, reference_id))) {
+				matched_pieces++;
+			}
+		}
+
+		if (matched_pieces == pieces.size()-1) {
+			seqan3::debug_stream << "exact match for '" << query << "' at position: " << match_position << "\n";
+		} else if (matched_pieces == pieces.size()-2) {
+			//seqan3::debug_stream << "Verifying partial match\n";
+			if (verify(reference[reference_id], query, (match_position-((piece_size*piece_id)+first_offset)), number_of_errors)) {
+				seqan3::debug_stream << "partial match for '" << query << "' found at position: " << match_position-(piece_size*piece_id) << "\n";
+			}
+		}
+	}
+
+    }
+
 
     return 0;
 }
